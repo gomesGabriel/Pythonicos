819a9aa560a91ff857b555ae42daa5d000b92b1e
Gabriel Gomes <gabrielgomesssilva14@gmai.com>
2018-08-25 09:39:01 -0300

    Tuplas

--- desafiosCursoEmVideo/ex021.py
+++ desafiosCursoEmVideo/ex021.py
@@ -4,14 +4,14 @@ pygame.mixer.music.load('ex021.mp3')
 pygame.mixer.music.play()
 input()
 pygame.event.wait()
+'''
+from pygame import *
+ixer.init()
+mixer.music.load('ex021.mp3')
+mixer.music.play()
+while mixer.music.get_busy():
+        time.Clock().tick(10)
 
-#from pygame import *
-#mixer.init()
-#mixer.music.load('ex021.mp3')
-#mixer.music.play()
-#while mixer.music.get_busy():
-#        time.Clock().tick(10)
-
-#import playsound
-#playsound.playsound('ex021.mp3', True)
-
+import playsound
+playsound.playsound('ex021.mp3', True)
+'''
